b1d43173afbd9619816b2af2f5cba4856b2bccdc
smallzooDev <smallzooDev@gmail.com>
2024-10-04 16:00:08 +0900

    hello serve

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 #![allow(unused)] // for turorial purpose
 
-use axum::Router;
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    // region:       --- Start Server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+    // end_region:   --- Start Server
 }
